8ee5328e3c12596a6f98dad983f58d76a1715994
Roman Zayev <semirook@gmail.com>
2014-03-14 17:04:19 +0200

    setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hsme',
+    version='0.1',
+    author='Roman Semirook',
+    author_email='semirook@gmail.com',
+    packages=find_packages(),
+    license='MIT',
+    url='https://github.com/semirook/hsme',
+    description='State machines for the web',
+    long_description='State machines for the web',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
